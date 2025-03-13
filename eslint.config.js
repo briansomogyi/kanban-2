a5064ebf08646f323e90f817bb77eab83f9d23ff
Radu Crisan <radu.crisan@ekkode.com>
2025-03-13 09:01:13 +0200

    add prettier and eslint configs

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import pluginVue from "eslint-plugin-vue"
+import pluginPrettier from "eslint-config-prettier"
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,vue}"] },
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-alert": "error"
+    }
+  },
+  pluginJs.configs.recommended,
+  ...pluginVue.configs["flat/recommended"],
+  pluginPrettier
+]
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from "./components/HelloWorld.vue"
+import { RouterLink, RouterView } from "vue-router"
+
+// alert("Salut!")
 </script>
 
 <template>
